0ee8c39e8214b29409bec4e13187d2db8576f83f
FrozenBuffalo1406 <ahmadfauzirizky003@gmail.com>
2023-08-12 14:46:29 +0700

    add cf contest beta round 4_A probs

---
+++ cf_codeForces-Beta-Round-4_A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int w;
+    cin >> w;
+    if (w % 2 == 0 && w != 2)
+        cout << "yes" << endl;
+    else
+        cout << "no" << endl;
+
+    return 0;
+}
